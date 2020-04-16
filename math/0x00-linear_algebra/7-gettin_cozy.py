0eadf118fe707b61edde7b4218cf7e38f0825804
yasheymateen <yasheymateen@gmail.com>
2020-04-16 06:48:11 +0000

    added getting cozy that concatenates two matrices along a specific axis

---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" function that concatenates two matrices along a specific axis"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    new_matrix = []
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        for row in mat1:
+            new_matrix.append(list(row))
+        for row in mat2:
+            new_matrix.append(list(row))
+    if axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        for row, element in zip(mat1, mat2):
+            new_matrix.append(list(row) + element)
+    return new_matrix
