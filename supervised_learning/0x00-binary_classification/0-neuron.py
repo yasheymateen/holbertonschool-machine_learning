48e18095d7191460b51042058dd7e16edf9eaef5
yasheymateen <yasheymateen@gmail.com>
2020-05-11 01:58:18 +0000

    added README file and first neuron file

---
+++ supervised_learning/0x00-binary_classification/0-neuron.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Neuron class that defines a single neuron performing binary classification """
+
+import numpy as np
+
+
+class Neuron:
+    """ neuron class """
+
+    def __init__(self, nx):
+        """ nx: number of input features to the neuron """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.W = np.ndarray((1, nx))
+        self.W[0] = np.random.normal(size=nx)
+        self.b = 0
+        self.A = 0
---
+++ supervised_learning/0x00-binary_classification/README.md
@@ -0,0 +1,3 @@
+## 0x00-Binary_Classification
+
+### Supervised Learning project that builds a binary image classifier from scratching using numpy
