251ecb05ad49c4f136d6d0d0885f8da960bc49cd
yasheymateen <yasheymateen@gmail.com>
2020-05-19 13:46:17 +0000

    solved syntax errors

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -13,34 +13,38 @@ forward_prop = __import__('2-forward_prop').forward_prop
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
           activations, alpha, iterations, save_path='/tmp/model.ckpt'):
     """ train our network """
-     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-     y_pred = forward_prop(x, layer_sizes, activations)
-     loss = calculate_loss(y, y_pred)
-     accuracy = calculate_accuracy(y, y_pred)
-     train = create_train_op(loss, alpha)
-     sess = tf.Session()
-     sess.run(tf.global_variables_initializer())
-     sess.run(tf.local_variables_initializer())
-     for iters in range(0, iterations):
-         if not iters % 100:
-             print('After {} iterations:'.format(iters))
-             trainloss, trainaccuracy = sess.run((loss, accuracy),
-                                                 feed_dict={x: X_train, y: Y_train})
-             print('\tTraining Cost:', trainloss)
-             print('\tTraining Accuracy:', trainaccuracy)
-             validloss, validaccuracy = sess.run((loss, accuracy),
-                                                 feed_dict={x, X_valid, y: Y_valid})
-             print('\tValidation Cost:', validloss)
-             print('\tValidation Accuracy:', validaccuracy)
+    x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+    y_pred = forward_prop(x, layer_sizes, activations)
+    loss = calculate_loss(y, y_pred)
+    accuracy = calculate_accuracy(y, y_pred)
+    train = create_train_op(loss, alpha)
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    sess.run(tf.local_variables_initializer())
+    for iters in range(0, iterations):
+        if not iters % 100:
+            print('After {} iterations:'.format(iters))
+            trainloss, trainaccuracy = sess.run((loss, accuracy),
+                                                feed_dict={x: X_train,
+                                                           y: Y_train})
+            print('\tTraining Cost:', trainloss)
+            print('\tTraining Accuracy:', trainaccuracy)
+            validloss, validaccuracy = sess.run((loss, accuracy),
+                                                feed_dict={x, X_valid,
+                                                           y: Y_valid})
+            print('\tValidation Cost:', validloss)
+            print('\tValidation Accuracy:', validaccuracy)
         sess.run(train, feed_dict={x: X_train, y: Y_train})
     if not iterations % 100:
         print('After {} iterations:'.format(iterations))
         trainloss, trainaccuracy = sess.run((loss, accuracy),
-                                            feed_dict={x: X_train, y: Y_train})
+                                            feed_dict={x: X_train,
+                                                       y: Y_train})
         print('\tTraining Cost:', trainloss)
         print('\tTraining Accuracy:', trainaccuracy)
         validloss, validaccuracy = sess.run((loss, accuracy),
-                                            feed_dict={x: X_valid, y: Y_valid})
+                                            feed_dict={x: X_valid,
+                                                       y: Y_valid})
         print('\tValidation Cost:', validloss)
         print('\tValidation Accuracy:', validaccuracy)
     saver = tf.train.Saver()
