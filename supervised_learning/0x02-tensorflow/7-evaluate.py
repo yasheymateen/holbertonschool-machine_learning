774fd337eec1e24acf75b36b932531c5f2b7b58f
yasheymateen <yasheymateen@gmail.com>
2020-09-22 06:05:38 +0000

    modified 7 evaluate file

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
-""" Evaluate restored model """
-
-
+""" Evaluate """
 import tensorflow as tf
 
 
 def evaluate(X, Y, save_path):
-    """ Evaluate restored model """
-    sess = tf.Session()
-    saver = tf.train.import_meta_graph(save_path + '.meta')
-    saver.restore(sess, save_path)
-    graph = tf.get_default_graph()
-    y_pred = graph.get_tensor_by_name("layer_2/BiasAdd:0")
-    accuracy = graph.get_tensor_by_name("Mean:0")
-    loss = graph.get_tensor_by_name("softmax_cross_entropy_loss/value:0")
-    x = graph.get_tensor_by_name("x:0")
-    y = graph.get_tensor_by_name("y:0")
-    return sess.run((y_pred, accuracy, loss), feed_dict={x: X, y: Y})
+    """ evaluates the output of a neural network.
+        X: (np.ndarray) containing the input data to evaluate.
+        Y: (np.ndarray) containing the one-hot labels for X.
+        save_path: the location to load the model from
+        Returns: the networks prediction, accuracy, and loss.
+    """
+    saver = tf.train.import_meta_graph("{}.meta".format(save_path))
+    with tf.Session() as sess:
+        saver.restore(sess, save_path)
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
+        pred = sess.run(y_pred, feed_dict={x: X, y: Y})
+        accu = sess.run(accuracy, feed_dict={x: X, y: Y})
+        loss_mod = sess.run(loss, feed_dict={x: X, y: Y})
+        return pred, accu, loss_mod
