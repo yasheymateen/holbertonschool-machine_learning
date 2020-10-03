563361fd56ee59c6ff5f014b1fe9f80207c79109
yasheymateen <yasheymateen@gmail.com>
2020-10-03 05:33:21 +0000

    modified 3-yolo py file

--- supervised_learning/0x0A-object_detection/3-yolo.py
+++ supervised_learning/0x0A-object_detection/3-yolo.py
@@ -13,7 +13,10 @@ class Yolo:
     def __init__(self, model_path, classes_path,
                  class_t, nms_t, anchors):
         self.model = K.models.load_model(model_path)
+        with open(classes_path) as class_file:
+            self.class_names = [line.rstrip('\n') for line in class_file]
         self.class_t = class_t
+        self.nms_t = nms_t
         self.anchors = anchors
 
     def process_outputs(self, outputs, image_size):
